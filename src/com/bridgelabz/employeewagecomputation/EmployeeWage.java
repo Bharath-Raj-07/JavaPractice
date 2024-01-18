9cdf2caf994ca6bc26bddc39a9f1c535d4f78aa5
Bharath-Raj-07 <bharathrajlifeline@gmail.com>
2024-01-18 21:24:15 +0530

    Added: Daily Wage

--- src/com/bridgelabz/employeewagecomputation/EmployeeWage.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWage.java
@@ -1,13 +1,21 @@
 package com.bridgelabz.employeewagecomputation;
 import java.lang.Math;
 public class EmployeeWage{
-    public static void main(String[] args) {
-        int Full_time = 1;
-        double empCheck = (Math.floor(Math.random() * 10) % 2);
-        if (empCheck == Full_time) {
-            System.out.println("Present");
-        } else {
-            System.out.println("Absent");
+    public static void main(String[] args){
+        int Full_time=1;
+        int PHRate=20;
+        int empHrs=0,empWage=0;
+        double empCheck;
+        empCheck = (Math.floor(Math.random()*10)%2);
+        if (empCheck == Full_time)
+        {
+            empHrs=8;
         }
+        else
+        {
+            empHrs=0;
+        }
+        empWage=empHrs*PHRate;
+        System.out.println("Emp Wage: "+empWage);
     }
 }
