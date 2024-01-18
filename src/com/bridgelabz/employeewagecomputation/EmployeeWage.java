2d3f1375082375aa8f0e3ff5af8599b5fcea6c91
Bharath-Raj-07 <bharathrajlifeline@gmail.com>
2024-01-18 21:39:50 +0530

    Added: Wage using Switch Case

--- src/com/bridgelabz/employeewagecomputation/EmployeeWage.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWage.java
@@ -1,24 +1,23 @@
 package com.bridgelabz.employeewagecomputation;
 import java.lang.Math;
 public class EmployeeWage{
+    public static final int Part_time=1;
+    public static final int Full_time=2;
+    public static final int PHRate=20;
     public static void main(String[] args){
-        int Part_time=1;
-        int Full_time=2;
-        int PHRate=20;
         int empHrs=0,empWage=0;
-        double empCheck;
-        empCheck = (Math.floor(Math.random()*10)%3);
-        if (empCheck == Part_time)
+        int empCheck;
+        empCheck = (int) (Math.floor(Math.random()*10)%3);
+        switch (empCheck)
         {
-            empHrs=4;
-        }
-        else if (empCheck == Full_time)
-        {
-            empHrs=8;
-        }
-        else
-        {
-            empHrs=0;
+            case Part_time:
+                empHrs=4;
+                break;
+            case Full_time:
+                empHrs=8;
+                break;
+            default:
+                empHrs=0;
         }
         empWage=empHrs*PHRate;
         System.out.println("Emp Wage: "+empWage);
