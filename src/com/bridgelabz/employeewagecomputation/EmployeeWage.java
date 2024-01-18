0dce37c350e1ed9ab3e1d76beb1f10991986b441
Bharath-Raj-07 <bharathrajlifeline@gmail.com>
2024-01-18 21:19:17 +0530

    Added: Attendance - UC1

--- src/com/bridgelabz/employeewagecomputation/EmployeeWage.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWage.java
@@ -1,5 +1,13 @@
 package com.bridgelabz.employeewagecomputation;
-
-public class EmployeeWage {
-
+import java.lang.Math;
+public class EmployeeWage{
+    public static void main(String[] args) {
+        int Full_time = 1;
+        double empCheck = (Math.floor(Math.random() * 10) % 2);
+        if (empCheck == Full_time) {
+            System.out.println("Present");
+        } else {
+            System.out.println("Absent");
+        }
+    }
 }
